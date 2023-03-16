304af1480ef32f6edb30e3787e1f4fb223da4bc5
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-03-15 22:34:49 -0300

    Consertado bug de atualização do LocalStorage

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", (evento) => {
         itemAtual.id = existe.id
 
         atualizaElemento(itemAtual)
-        itemAtual[existe] = itemAtual;
+        itens[existe.id] = itemAtual;
     } else {
         itemAtual.id = itens.length
 
